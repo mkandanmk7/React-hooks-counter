e2328b2906f91a8b6fd285db988d781d3fd29973
muthu <mkandanmk7@gmail.com>
2021-09-13 22:42:36 +0530

    hooks counter

--- src/App.js
+++ src/App.js
@@ -3,30 +3,34 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const inc = () => {
-    console.log("incremented");
+    // console.log("incremented");
     setCount(count + 1);
   };
 
   const dec = () => {
-    console.log("Decremented");
+    // console.log("Decremented");
+    if (count > 0) setCount(count - 1);
   };
 
   const reset = () => {
-    console.log("reseted");
+    // console.log("reseted");
+    setCount(0);
   };
 
   return (
-    <div className="App">
-      <h1>Fucntional Counter</h1>
-      <div className="counter">
-        <p>Counter : {count}</p>
+    <>
+      <div className="App">
+        <h1>Fucntional Counter</h1>
+        <div className="counter">
+          <p>Counter : {count}</p>
+        </div>
+        <div className="btns">
+          <button onClick={inc}>Increment</button>
+          <button onClick={dec}>Decrement</button>
+          <button onClick={reset}>Reset</button>
+        </div>
       </div>
-      <div className="btns">
-        <button onClick={() => inc()}>Increment</button>
-        <button onClick={() => dec()}>Decrement</button>
-        <button onClick={() => reset()}>Reset</button>
-      </div>
-    </div>
+    </>
   );
 }
 
